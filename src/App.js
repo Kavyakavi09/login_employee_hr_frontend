5cafb1c4a9869ca13cb74ca80f0fe22fb76fc608
Kavya K <95955787+Kavyakavi09@users.noreply.github.com>
2022-08-27 11:11:32 +0530

    hashroute

--- src/App.js
+++ src/App.js
@@ -2,21 +2,21 @@ import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
 import EmployeeHrSignin from './components/EmployeeHrSignin';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import EmployeeHrSignUp from './pages/EmployeeHrSignup';
 import EmployeeDashboard from './pages/EmployeeDashboard';
 import HrDashboard from './pages/HrDashboard';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path='/' element={<EmployeeHrSignin />} />
         <Route path='/signup' element={<EmployeeHrSignUp />} />
         <Route path='/employee-dashboard' element={<EmployeeDashboard />} />
         <Route path='/hr-dashboard' element={<HrDashboard />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
